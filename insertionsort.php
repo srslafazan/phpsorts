82a716c31deabaf1f81b49ee2c4e1066e3193716
Shain Lafazan <srslafazan@gmail.com>
2015-04-13 21:29:38 -0700

    added selectionsort.php

---
+++ insertionsort.php
@@ -0,0 +1,47 @@
+<?php
+
+
+    $array = array();
+
+    for ($k = 0 ; $k < 1000 ; $k++) {
+        array_push($array, rand(0, 10000));
+    }
+
+    function microtime_float()
+    {
+        list($usec, $sec) = explode(" ", microtime());
+        return ((float)$usec + (float)$sec);
+    }
+
+    $time_start = microtime_float();
+
+
+    function insertionsort($a) {
+
+        for ($i = 0 ; $i < count($a)-1 ; $i++ ) { 
+            for ( $j = $i + 1 ; $j > 0 ; $j-- ) {
+                if ( $a[$j] < $a[$j - 1 ] ) {
+
+                    $t = $a[$j];
+                    $a[$j] = $a[$j-1];
+                    $a[$j-1] = $t;
+                }
+            }
+        }
+        return $a;
+    }
+
+
+    var_dump(insertionsort($array));
+
+    $time_end = microtime_float();
+    $time = $time_end - $time_start;
+
+    echo "Did nothing in $time seconds\n";
+
+// .0069 seconds for 100 random elements
+// over 30 seconds for 10,000 random elements
+
+// bigO(n^2) time complexity
+
+?>
